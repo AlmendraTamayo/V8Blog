05ef86b7de25c0696dfe24fbf4b5b8b3b955cc55
AlmendraTamayo <almendra.tamayo@gmail.com>
2021-08-24 21:47:13 -0400

    se sube proyecto

---
+++ src/components/AdministradorAvanzado.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header class="masthead" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto">
+            <div class="post-heading">
+              <h1><strong>Chevrolet presenta la totalmente nueva Blazer</strong></h1>
+              <h2 class="subheading">
+                <strong>Esta página de administración está en construcción. Intente como administrador simple.</strong>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Inicio from './components/Inicio'
-import SobreMi from './components/SobreMi'
-import Contacto from './components/Contacto'
-import Post from './components/Post'
-import Articulo from './components/Articulo'
+//import Inicio from './components/Inicio'
+//import SobreMi from './components/SobreMi'
+//import Contacto from './components/Contacto'
+//import Post from './components/Post'
+//import Articulo from './components/Articulo'
 import NotFound from './components/NotFound'
 Vue.use(Router)
 
@@ -14,28 +14,59 @@ export default new Router({
         {
             path: '/',
             name: 'inicio',
-            component: Inicio
+            component: ()=>import("./components/Inicio")
         },
         {
             path: '/sobremi',
             name: 'sobremi',
-            component: SobreMi
+            alias:["/acerca"],
+            component: ()=>import("./components/SobreMi")
         },
         {
             path: '/contacto',
             name: 'contacto',
-            component: Contacto
+            alias:["/contactame"],
+            component: ()=>import("./components/Contacto")
         },
         {
             path: '/post',
             name: 'post',
-            component: Post,
+            component: ()=>import("./components/Post"),
+        
             children: [
                 {
                     path: ':articulo',
-                    component: Articulo,
+                    component: ()=>import("./components/Articulo"),
                 }
             ]
+        },{
+            path: "/administrador",
+            name: "administrador",
+            component: ()=>import("./components/Administrador"),
+            children:[
+                {
+                    path: "/*simple",
+                    name: "administrador-simple",
+                    component: ()=>import("./components/AdministradorSimple.vue")
+                },
+                {
+                    path:"/*avanzado",
+                    name:"administrador-avanzado",
+                    component: ()=>import("./components/AdministradorAvanzado.vue")
+                },
+            ]
+        },
+        {
+            path: "/home",
+             redirect: "/"
+},
+        {
+            path: "/inicio",
+            redirect: "/"
+        },
+        {
+            path:"/portada",
+            redirect: "/"
         },
         {
             path: '*',
